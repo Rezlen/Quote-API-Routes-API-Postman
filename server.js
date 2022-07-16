d7a091b6b36d90698acefd12ad455a416b934418
Rezlen <soheilyreza@yahoo.com>
2022-07-16 16:57:43 +0100

    Comments added

--- server.js
+++ server.js
@@ -9,12 +9,14 @@ const PORT = process.env.PORT || 4001;
 
 app.use(express.static('public'));
 
+// Your API should have a GET /api/quotes/random route. This route should send back a random quote from the quotes data.
 app.get('/api/quotes/random', (req, res) => {
   res.send({
     quote: getRandomElement(quotes)
   });
 });
 
+// Your API should have a GET /api/quotes route. This route should return all quotes from the data if the request has no query params. If there is a query string with a person attribute, the route should return all quotes said by the same person. For instance, the data set has multiple quotes for Grace Hopper, so GET /api/quotes?person=Grace Hopper should return an array of only those quotes. If there are no quotes for the requested person, send back an empty array.
 app.get('/api/quotes', (req, res) => {
   if (req.query.person !== undefined) {
     const quotesByPerson = quotes.filter(quote => quote.person === req.query.person);
@@ -28,6 +30,7 @@ app.get('/api/quotes', (req, res) => {
   }
 });
 
+// Your API should have a POST /api/quotes route for adding new quotes to the data. New quotes will be passed in a query string with two properties: quote with the quote text itself, and person with the person who is credited with saying the quote. This route should verify that both properties exist in the request query string and send a 400 response if it does not. If all is well, this route handler should add the new quote object to the data array and send back a response
 app.post('/api/quotes', (req, res) => {
   const newQuote = {
     quote: req.query.quote,
@@ -40,6 +43,7 @@ app.post('/api/quotes', (req, res) => {
     res.status(400).send();
   }
 });
+
 
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}.`);
